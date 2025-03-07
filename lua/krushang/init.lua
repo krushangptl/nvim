039632f870e6466a247670b8ac8d6263ec030ac6
Krushang D Patel <krushang4201@gmail.com>
2025-03-07 19:00:22 +0530

    clangd, well i like it

--- lua/krushang/init.lua
+++ lua/krushang/init.lua
@@ -75,7 +75,7 @@ require("nvim-ts-autotag").setup({
 -- === Mason configuration ===
 require("mason").setup()
 require("mason-lspconfig").setup({
-    ensure_installed = { "lua_ls", "ruff", "ts_ls" }, -- Add LSPs you need
+    ensure_installed = { "lua_ls", "pyright", "ruff", "ts_ls", "clangd" }, -- Add LSPs you need
     automatic_installation = true,
 })
 
