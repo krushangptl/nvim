d6f76ebdd4d844202ff9ce5a9e3965b54b7bbc8b
Krushang D Patel <krushang4201@gmail.com>
2026-03-01 21:19:34 +0530

    add kanagawa theme

--- lua/krushangptl/lazy/color.lua
+++ lua/krushangptl/lazy/color.lua
@@ -10,7 +10,13 @@ return {
         "rose-pine/neovim",
         name = "rose-pine",
         config = function()
-            vim.cmd("colorscheme rose-pine")
+            -- vim.cmd("colorscheme rose-pine")
         end
-    }
+    },
+    {
+        "rebelot/kanagawa.nvim",
+        config = function()
+            vim.cmd("colorscheme kanagawa-dragon")
+        end
+    },
 }
