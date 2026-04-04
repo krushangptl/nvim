c035a6a51dd36c4b3296c84675e24883d552a6e8
Krushang D Patel <krushang4201@gmail.com>
2026-04-04 16:53:23 +0530

    locked in

--- lua/krushangptl/lazy/color.lua
+++ lua/krushangptl/lazy/color.lua
@@ -1,22 +1,58 @@
 return {
-    {
-        "blazkowolf/gruber-darker.nvim",
-        priority = 1000,
-        config = function()
-            vim.cmd.colorscheme("gruber-darker")
-        end,
-    },
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        config = function()
-            -- vim.cmd("colorscheme rose-pine")
-        end
-    },
-    {
-        "rebelot/kanagawa.nvim",
-        config = function()
-            -- vim.cmd("colorscheme kanagawa-dragon")
-        end
-    },
+	{
+		"blazkowolf/gruber-darker.nvim",
+		priority = 1000,
+		config = function()
+			-- vim.cmd.colorscheme("gruber-darker")
+		end,
+	},
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function()
+			-- vim.cmd("colorscheme rose-pine")
+		end,
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		config = function()
+			-- vim.cmd("colorscheme kanagawa-dragon")
+		end,
+	},
+	{
+		"nyoom-engineering/oxocarbon.nvim",
+		-- Add in any other configuration;
+		--   event = foo,
+		--   config = bar
+		--   end,
+		config = function()
+			-- vim.cmd("colorscheme oxocarbon")
+		end,
+	},
+	{
+		"vague-theme/vague.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other plugins
+		config = function()
+			-- NOTE: you do not need to call setup if you don't want to.
+			require("vague").setup({
+				-- optional configuration here
+			})
+			-- vim.cmd("colorscheme vague")
+		end,
+	},
+	{
+		"zenbones-theme/zenbones.nvim",
+		-- Optionally install Lush. Allows for more configuration or extending the colorscheme
+		-- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+		-- In Vim, compat mode is turned on as Lush only works in Neovim.
+		dependencies = "rktjmp/lush.nvim",
+		lazy = false,
+		priority = 1000,
+		-- you can set set configuration options here
+		config = function()
+			vim.g.zenbones_darken_comments = 45
+			vim.cmd.colorscheme("zenbones")
+		end,
+	},
 }
--- lua/krushangptl/lazy/nonels.lua
+++ lua/krushangptl/lazy/nonels.lua
@@ -1,9 +1,11 @@
 return {
 	"nvimtools/none-ls.nvim",
 	dependencies = { "nvim-lua/plenary.nvim" },
+
 	config = function()
 		local null_ls = require("null-ls")
 
+		-- Setup null-ls
 		null_ls.setup({
 			sources = {
 				-- Lua
@@ -14,10 +16,20 @@ return {
 			},
 		})
 
-		-- format on save (optional but you want it)
+		-- Create augroup to avoid duplicate autocmds
+		local group = vim.api.nvim_create_augroup("FormatOnSave", { clear = true })
+
+		-- Format on save (triggered by :w)
 		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = group,
+			pattern = { "*.lua", "*.py" }, -- restrict to your formatters
 			callback = function()
-				vim.lsp.buf.format({ async = false })
+				vim.lsp.buf.format({
+					async = false, -- block until formatting finishes
+					filter = function(client)
+						return client.name == "null-ls" -- only use null-ls
+					end,
+				})
 			end,
 		})
 	end,
